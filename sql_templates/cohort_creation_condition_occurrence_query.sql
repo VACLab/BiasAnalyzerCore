df677d47025a2617bd25bfbd677c9d7f1f7097f0
hyi <hongyi@renci.org>
2025-01-15 12:55:37 -0500

    updated sql template to conform to the updated yaml file

--- sql_templates/cohort_creation_condition_occurrence_query.sql
+++ sql_templates/cohort_creation_condition_occurrence_query.sql
@@ -3,7 +3,7 @@ SELECT c.person_id,
        c.condition_end_date as cohort_end_date
 FROM condition_occurrence c
 JOIN person p ON c.person_id = p.person_id
-WHERE c.condition_concept_id = {{ inclusion_criteria.condition_occurrence.condition_concept_id }}
+WHERE 1=1
 {% if inclusion_criteria.demographics %}
     {% if inclusion_criteria.demographics.gender == 'female' %}
       AND p.gender_concept_id = 8532
@@ -18,44 +18,84 @@ WHERE c.condition_concept_id = {{ inclusion_criteria.condition_occurrence.condit
     {% endif %}
 {% endif %}
 
-    {% if inclusion_criteria.temporal_events %}
-    AND c.person_id IN (
-        {% for event in inclusion_criteria.temporal_events %}
-            {% if loop.index == 1 %}
-            SELECT person_id
-            FROM (
-                SELECT person_id,
-                       ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY visit_start_date ASC) AS event_instance
-                FROM visit_occurrence
-                WHERE visit_concept_id = {{ event.event_concept_id }}
-            ) seq
-            WHERE seq.event_instance = {{ event.event_instance }}
-            {% else %}
-            {% if event.operator == 'NOT' %}
-            AND NOT EXISTS (
-                SELECT person_id
-                FROM (
-                    SELECT person_id,
-                           ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY visit_start_date ASC) AS event_instance
-                    FROM visit_occurrence
-                    WHERE visit_concept_id = {{ event.event_concept_id }}
-                ) seq
-                WHERE seq.event_instance = {{ event.event_instance }}
-            )
-            {% else %}
-            {{ event.operator }}
-            SELECT person_id
-            FROM (
-                SELECT person_id,
-                       ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY visit_start_date ASC) AS event_instance
-                FROM visit_occurrence
-                WHERE visit_concept_id = {{ event.event_concept_id }}
-            ) seq
-            WHERE seq.event_instance = {{ event.event_instance }}
-            {% endif %}
-            {% endif %}
-        {% endfor %}
-    )
+{% if inclusion_criteria.temporal_events %}
+    {% for event_group in inclusion_criteria.temporal_events %}
+        {% if event_group.operator == 'AND' %}
+            {% for event in event_group.events %}
+                {% if event.event_type == 'condition_occurrence' %}
+                    {% if loop.first %}
+                        AND c.person_id IN (
+                    {% endif %}
+                        SELECT person_id
+                        FROM condition_occurrence
+                        WHERE person_id = c.person_id
+                            AND condition_concept_id = {{ event.condition_concept_id }}
+                    {% if loop.last %}
+                        )
+                    {% endif %}
+                {% elif event.event_type == 'visit_occurrence' %}
+                    {% if loop.first %}
+                        AND c.person_id IN (
+                    {% endif %}
+                        SELECT person_id
+                        FROM visit_occurrence
+                        WHERE person_id = c.person_id
+                          AND visit_concept_id = {{ event.event_concept_id }}
+                          AND event_instance = {{ event.event_instance }}
+                    {% if loop.last %}
+                        )
+                    {% endif %}
+                {% endif %}
+            {% endfor %}
+        {% elif event_group.operator == 'OR' %}
+            {% for event in event_group.events %}
+                {% if event.event_type == 'condition_occurrence' %}
+                    {% if loop.first %}
+                        AND c.person_id IN (
+                    {% endif %}
+                        SELECT person_id
+                        FROM condition_occurrence
+                        WHERE person_id = c.person_id
+                          AND condition_concept_id = {{ event.condition_concept_id }}
+                    {% if loop.last %}
+                        )
+                    {% endif %}
+                {% elif event.event_type == 'visit_occurrence' %}
+                    {% if loop.first %}
+                        AND c.person_id IN (
+                    {% endif %}
+                        SELECT person_id
+                        FROM visit_occurrence
+                        WHERE person_id = c.person_id
+                          AND visit_concept_id = {{ event.event_concept_id }}
+                          AND event_instance = {{ event.event_instance }}
+                    {% if loop.last %}
+                        )
+                    {% endif %}
+                {% endif %}
+            {% endfor %}
+        {% elif event_group.operator == 'NOT' %}
+            {% for event in event_group.events %}
+                {% if event.event_type == 'condition_occurrence' %}
+                    AND NOT EXISTS (
+                        SELECT 1
+                        FROM condition_occurrence ex
+                        JOIN person ep ON ex.person_id = ep.person_id
+                        WHERE ex.person_id = c.person_id
+                            AND ex.condition_concept_id = {{ event.condition_concept_id }}
+                    )
+                {% elif event.event_type == 'visit_occurrence' %}
+                    AND NOT EXISTS (
+                        SELECT 1
+                        FROM visit_occurrence ex
+                        JOIN person ep ON ex.person_id = ep.person_id
+                        WHERE ex.person_id = c.person_id
+                            AND ex.visit_concept_id = {{ event.event_concept_id }}
+                    )
+                {% endif %}
+            {% endfor %}
+        {% endif %}
+    {% endfor %}
 {% endif %}
 
 {% if exclusion_criteria %}
@@ -63,16 +103,55 @@ AND NOT EXISTS (
     SELECT 1
     FROM condition_occurrence ex
     JOIN person ep ON ex.person_id = ep.person_id
-    WHERE   ex.person_id = c.person_id
-            AND ex.condition_concept_id = {{ exclusion_criteria.condition_occurrence.condition_concept_id }}
-            {% if exclusion_criteria.demographics %}
-                {% if exclusion_criteria.demographics.min_birth_year %}
-                AND ep.year_of_birth >= {{ exclusion_criteria.demographics.min_birth_year }}
-                {% endif %}
-                {% if exclusion_criteria.demographics.max_birth_year %}
-                AND ep.year_of_birth <= {{ exclusion_criteria.demographics.max_birth_year }}
-                {% endif %}
+    WHERE ex.person_id = c.person_id
+        {% if exclusion_criteria.demographics %}
+            {% if exclusion_criteria.demographics.min_birth_year %}
+            AND ep.year_of_birth >= {{ exclusion_criteria.demographics.min_birth_year }}
             {% endif %}
+            {% if exclusion_criteria.demographics.max_birth_year %}
+            AND ep.year_of_birth <= {{ exclusion_criteria.demographics.max_birth_year }}
+            {% endif %}
+        {% endif %}
+    {% if exclusion_criteria.temporal_events %}
+        {% for event_group in exclusion_criteria.temporal_events %}
+            {% if event_group.operator == 'AND' %}
+                {% for event in event_group.events %}
+                    {% if event.event_type == 'condition_occurrence' %}
+                        AND ex.condition_concept_id = {{ event.condition_concept_id }}
+                    {% elif event.event_type == 'visit_occurrence' %}
+                        AND ex.visit_concept_id = {{ event.event_concept_id }}
+                        AND ex.event_instance = {{ event.event_instance }}
+                    {% endif %}
+                {% endfor %}
+            {% elif event_group.operator == 'OR' %}
+                {% for event in event_group.events %}
+                    {% if event.event_type == 'condition_occurrence' %}
+                        AND ex.condition_concept_id = {{ event.condition_concept_id }}
+                    {% elif event.event_type == 'visit_occurrence' %}
+                        AND ex.visit_concept_id = {{ event.event_concept_id }}
+                        AND ex.event_instance = {{ event.event_instance }}
+                    {% endif %}
+                {% endfor %}
+            {% elif event_group.operator == 'NOT' %}
+                {% for event in event_group.events %}
+                    {% if event.event_type == 'condition_occurrence' %}
+                        AND NOT EXISTS (
+                            SELECT 1
+                            FROM condition_occurrence ex
+                            WHERE ex.person_id = c.person_id
+                                AND ex.condition_concept_id = {{ event.condition_concept_id }}
+                        )
+                    {% elif event.event_type == 'visit_occurrence' %}
+                        AND NOT EXISTS (
+                            SELECT 1
+                            FROM visit_occurrence ex
+                            WHERE ex.person_id = c.person_id
+                                AND ex.visit_concept_id = {{ event.event_concept_id }}
+                        )
+                    {% endif %}
+                {% endfor %}
+            {% endif %}
+        {% endfor %}
+    {% endif %}
 )
 {% endif %}
-
